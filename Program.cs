095d620cffe8753b5cafb7be18e6d20e96d58196
Robert Polak <66738761+polakr@users.noreply.github.com>
2022-06-20 11:45:17 +0200

    Basic healthcheck - dummy does not really check anything

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//for the health checks see
+//https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-6.0&viewFallbackFrom=aspnetcore-2.2
+//liveness probe - at this point we are always ok
+builder.Services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy());
+
 
 var app = builder.Build();
 
@@ -14,7 +22,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//http redirection should not be enabled for API
+//https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-6.0&tabs=visual-studio
+//app.UseHttpsRedirection();
 
 var summaries = new[]
 {
@@ -34,6 +44,21 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+//registration of liveness (self check)
+app.MapHealthChecks("/health/liveness", new HealthCheckOptions
+{
+    Predicate = r => r.Name.Contains("self")
+}).RequireHost("*:9090");
+
+//registration of readiness
+app.MapHealthChecks("/health/readiness", new HealthCheckOptions
+{
+    Predicate = r => r.Tags.Contains("services")
+}).RequireHost("*:9090");
+
+
+
 
 app.Run();
 
